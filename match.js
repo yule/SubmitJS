7065f9dcfb369d8eff244099e8eae63b9530cb00
Yule <paul@dryule.com>
2011-08-23 12:33:09 +0100

    adding the good stuff

--- match.js
+++ match.js
@@ -1,6 +1,16 @@
 window.addEventListener("keydown", function(event) {
   var controlKey = event.ctrlKey || event.metaKey;
   if (controlKey && event.keyCode == 13) {
-    alert('yo');
+    //make sure only one form...
+	if (document.forms.length == 1){
+		document.forms[0].submit();
+	}else if (document.forms.length == 0){
+	  //do nothing.
+	}else{
+	  //try and match active element:
+	  if (document.activeElement.form){
+	    document.activeElement.form.submit();
+	  }
+	}
   }
 }, false);
